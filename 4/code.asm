e0f45a3c3fa76719133edcad514bbf0c6c112c89
Daulet <daulettbot@gmail.com>
2019-05-22 03:06:29 +0300

    fix: used BIOS int

--- 4/code.asm
+++ 4/code.asm
@@ -1,56 +1,34 @@
-; simple adder of two hex number (5 char length)
+#make_boot#
 
-data segment
-    s1 db 'Slag1 = $'
-    s2 db 0dh, 0ah, 'Slag2 = $'
-    s3 db 0dh, 0ah, 'Summa: $'
-    se db 0dh, 0ah, 'some error$'
-    ; buffer 1 contains slag1 and answer after calc
-    b1 db 6         ; buffer size
-        db 00       ; buffer pointer
-        db 6 dup(0) ; buffer itself
-    ; buffer 2 contains slag2  
-    b2 db 6         ; buffer2 size
-        db 00       ; buffer2 pointer
-        db 6 dup(0) ; buffer2 itself
-ends
+org 7c00h      ; set location counter.
 
-code segment
-start:
-    ; set segment registers:
-    mov ax, data
-    mov ds, ax
-    mov es, ax
+; CODE
+    
+    lea si, s1
+    call OUTPUT
 
-    ; print "slag1 = "        
-    lea dx, s1
-    mov ah, 9
-    int 21h
+    lea di, b1
+    call INPUT
     
-    ; input slag    
-    mov ah, 0ah
-    mov dx, offset b1
-    int 21h
+    call NEW_LINE
     
-    ; print "slag2 = "
-    lea dx, s2
-    mov ah, 9
-    int 21h
+    lea si, s2
+    call OUTPUT
+     
+    lea di, b2
+    call INPUT
     
-    ; input slag2    
-    mov ah, 0ah
-    mov dx, offset b2
-    int 21h
+    call NEW_LINE
     
-    ; print "summa: "
-    lea dx, s3
-    mov ah, 9
-    int 21h
-
-    ; ------Calculation phase------    
-    ; -----------------------------
-    mov dh, 0    ; addition one stored here
+    lea si, sm
+    call OUTPUT
     
+    
+    call CALC
+    
+    jmp EXIT
+    
+GET_SLAG:
     lea di, b1   ; b1 stored here
     inc di
     mov bl, [di] ; bl = size of b1
@@ -59,11 +37,10 @@ start:
     lea si, b2   ; b2 stored here
     inc si
     mov al, [si] ; al = size of b2
-    mov ah, 0
     
     ; if second number has more digits
     cmp al, bl
-    jle CALC
+    jle GET_SLAG_RET
     ; then we will swap src and dst
     lea di, b2
     inc di
@@ -73,116 +50,45 @@ start:
     lea si, b1
     inc si
     mov al, [si] ; al = size of b1
+GET_SLAG_RET:
     mov ah, 0
-    
+    ret    
     
 CALC:
+    call GET_SLAG
     add di, bx   ; move to last digit
     add si, ax
-    
 CALC_LOOP:
-    ; parse char of b1 to hex
-    mov ch, 2h
-    mov cl, [si]
-    jmp PARSE
-DECIMAL:
-    sub cl, 30h
-    dec ch
-    jnz PARSE1_DONE
-    jmp PARSE2_DONE
-HEX1:
-    sub cl, 37h
-    dec ch
-    jnz PARSE1_DONE
-    jmp PARSE2_DONE
-HEX2:
-    sub cl, 57h
-    dec ch
-    jnz PARSE1_DONE
-    jmp PARSE2_DONE
-PARSE:
-    cmp cl, 30h ; if less 0 
-    jl ERROR 
-    cmp cl, 39h ; if less or equal 9
-    jle DECIMAL
-    cmp cl, 41h ; if less A
-    jl ERROR
-    cmp cl, 46h ; if less or equal F
-    jle HEX1
-    cmp cl, 61h ; if less a
-    jl ERROR
-    cmp cl, 66h ; if less or equal f
-    jle HEX2
-ERROR:
-    lea dx, se
-    mov ah, 9
-    int 21h
-    jmp EXIT
-PARSE1_DONE:
-    mov [si], cl    
-    mov cl, [di]
-    jmp PARSE
-PARSE2_DONE:
-    mov [di], cl
     cmp ax, 0h
-    je LAST_CHAR    
+    je LAST_CHAR
+    mov cl, [di]
     mov dl, [si]
-    add cl, dl   ; add slags 
+    add cl, dl
 LAST_CHAR:
-    add cl, dh   ; add one if exist
-    mov dh, 0h
-    cmp cl, 0fh  ; additional one?
+    add cl, ch
+    mov ch, 0h
+    cmp cl, 0fh
     jle CALC_RES
-    mov dh, 1h
-    sub cl, 10h    
+    mov ch, 1h
+    sub cl, 10h
 CALC_RES:
     mov [di], cl
     dec di
-    dec si
     dec bx
     cmp ax, 0h
     je CALC_END
+    dec si
     dec ax
     jnz CALC_LOOP
-    cmp dh, 0h
+    cmp ch, 0h
     je CALC_END
-    mov ch, 1h
     mov cl, [di]
     cmp bx, 0h
-    jne PARSE
-    ; print digit
-    mov ah, 2
-    mov dl, 31h
-    int 21h 
-CALC_END:    
-    
-    ; checking the answer
-    lea di, b1   ; b1 stored here
-    inc di
-    mov bl, [di] ; bl = size of b1
-    mov bh, 0
-    
-    lea si, b2   ; b2 stored here
-    inc si
-    mov al, [si] ; al = size of b2
-    mov ah, 0
-    
-    ; if second number has more digits
-    cmp al, bl
-    jle PARSE_BACK
-    ; then we will swap src and dst
-    lea di, b2
-    inc di
-    mov bl, [di] ; bl = size of b2
-    mov bh, 0
-    
-    lea si, b1
-    inc si
-    mov al, [si] ; al = size of b1
-    mov ah, 0
-    
-    ; parse hex to char
-PARSE_BACK:
+    jne LAST_CHAR
+    mov al, 31h
+    call OUTPUT_CHAR        
+CALC_END:
+    call GET_SLAG
     inc di
 LOOP1:
     mov al, [di]
@@ -207,17 +113,130 @@ PARSE_BACK1:
 NEXT:
     mov [si], al
     ; print digit
-    mov ah, 2
-    mov dl, al
-    int 21h 
+    call OUTPUT_CHAR
     inc di 
     dec bl 
     jnz LOOP1
-      
-EXIT:
-    ; exit to operating system.
-    mov ax, 4c00h
-    int 21h    
-ends
+    ret
+    
+OUTPUT_CHAR:
+    mov ah, 0eh
+    mov bh, 0h
+    int 10h
+    mov ah, 02h
+    inc [col]
+    cmp [col], 30h
+    jl  OLD_LINE
+    mov [col], 0h
+    inc [row]
+OLD_LINE:
+    mov dh, [row]
+    mov dl, [col]    
+    int 10h
+    ret
+NEW_LINE:
+    mov ah, 02h
+    inc [row]
+    mov [col], 0h
+    mov dh, [row]
+    mov dl, [col]    
+    int 10h
+    ret 
+OUTPUT:
+    mov al, [si]
+    cmp al, 0h
+    je OUTPUT_END
+    call OUTPUT_CHAR
+    inc si
+    jmp OUTPUT
+OUTPUT_END:
+    ret
+    
+INPUT:
+    mov cl, [di]
+    inc di
+    mov bl, [di]
+    mov bh, 0h
+INPUT_LOOP:    
+    cmp cl, bl
+    je INPUT_END
+    call INPUT_CHAR
+    cmp al, 0dh
+    je INPUT_END 
+    ; save
+    inc bl
+    add di, bx
+    mov [di], al
+    sub di, bx
+    mov [di], bl
+    ; print
+    mov al, ah
+    mov ah, 0eh
+    int 10h
+ mov ah, 02h
+ inc [col]
+    cmp [col], 30h
+    jl  OLD_LINE2
+    mov [col], 0h
+    inc [row]
+OLD_LINE2:
+    mov dh, [row]
+    mov dl, [col]    
+    int 10h
+    jmp INPUT_LOOP 
+INPUT_END:    
+    cmp bl, 0h
+    je ERROR
+    ret
+INPUT_CHAR_END:
+    ret
+INPUT_CHAR:    
+    mov ah, 0h
+    int 16h
+    cmp al, 0dh ; if enter
+    je INPUT_CHAR_END
+    mov ah, al
+    cmp al, 30h ; if less 0 
+    jl ERROR 
+    cmp al, 39h ; if less or equal 9
+    jle DECIMAL
+    cmp al, 41h ; if less A
+    jl ERROR
+    cmp al, 46h ; if less or equal F
+    jle HEX1
+    cmp al, 61h ; if less a
+    jl ERROR
+    cmp al, 66h ; if less or equal f
+    jle HEX2
+ERROR:
+    lea dx, se
+    mov ah, 9
+    int 21h        ; fix
+    jmp EXIT
+DECIMAL:
+    sub al, 30h
+    ret
+HEX1:
+    sub al, 37h
+    ret
+HEX2:
+    sub al, 57h
+    ret
 
-end start ; set entry point and stop the assembler.
+; DATA
+s1 db 'Slag1 = ', 0h
+s2 db 'Slag2 = ', 0h
+sm db 'Summa: ', 0h
+se db 'some error', 0h
+
+b1 db 6         ; buffer size
+    db 00       ; buffer pointer
+    db 6 dup(0) ; buffer itself
+b2 db 6         ; buffer size
+    db 00       ; buffer pointer
+    db 6 dup(0) ; buffer itself
+col db 0h       ; coursor col saved here
+row db 0h       ; coursor row saved here
+
+EXIT:
+    INT 19h        ; reboot
